f72159610f9dc7240834b0b1eaada99fc28c5089
Yusif573 <160158005+Yusif573@users.noreply.github.com>
2024-03-03 19:00:27 +0400

    Add files via upload

---
+++ InstagramPost.cs
@@ -0,0 +1,29 @@
+﻿using InstagramHW.AdminPanel;
+
+namespace InstagramHW;
+
+public class InstagramPostClass
+{
+    static public int _staticPostId {  get; set; }
+    public int _postId {  get; set; }
+    public AdminClass Admin {  get; set; }
+    public string? Content {  get; set; }
+    public DateTime CreationDateTime {  get; set; }
+    public uint LikeCount {  get; set; }
+    public uint ViewCount {  get; set; }
+    public InstagramPostClass()
+    {
+        _postId = _staticPostId++;
+    }
+    public InstagramPostClass(string content,AdminClass admin)
+        :this()
+    {
+        Admin = admin;
+        Content = content;
+        CreationDateTime = DateTime.Now;
+    }
+    public string showPost()
+    {
+        return $"Id:{_postId}\nAdmin:{Admin.Username}\n{Content}\n{CreationDateTime}\nLikes:{LikeCount}\tViews:{ViewCount}";
+    }
+}
