4430e365a38a9a67b9104763f763b2d8a8e3581d
Yusif573 <160158005+Yusif573@users.noreply.github.com>
2024-03-03 18:59:18 +0400

    Add files via upload

---
+++ NotificationClass.cs
@@ -0,0 +1,28 @@
+﻿using InstagramHW.UserPanel;
+
+namespace InstagramHW.Notification;
+
+public class NotificationClass
+{
+    //id,Text,DateTime,FromUser(bu hansi user terefinden bu bildirishin geldiyidir)
+    static public int _notificationStaticId {  get; set; }
+    public int _notificationId { get; set; }
+    public string? Text {  get; set; }
+    public DateTime NotificationDate { get; set; }
+    public UserClass FromUser { get; set; }
+    public NotificationClass()
+    {
+        _notificationId = _notificationStaticId++;
+    }
+    public NotificationClass(string text,DateTime notificationDate,UserClass user)
+        :this()
+    {
+        Text = text;
+        NotificationDate = notificationDate;
+        FromUser = user;
+    }
+    public string showNotification()
+    {
+        return $"Id:{_notificationId}\nDate time:{NotificationDate}\nUser:{FromUser.Username}";
+    }
+}
